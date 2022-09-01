f7c64ab84d6fa799ee04097d2595b9fc2cfd750f
Kurochkin Rostislav <r.kurr@mail.ru>
2022-09-01 22:19:54 +1000

    feat: js popup / refactor: img / fix: css

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
-console.log("Helo, Pedr");
+console.log("Helo, epta");
 
-const popup = DocumentType
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button-img');
+const closeButton = document.querySelector('.popup__button-close');
+const saveButton = document.querySelector('popup__button-save');
+
+const togglePopup = function () {
+    popup.classList.toggle('popup_opened');
+}
+
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
