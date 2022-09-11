2f06eca9c957a18feefa0bfe1dc310e5be9a30c8
Rostislavgit config --global user.name Rostislavgit config --global user.name Rostislav <ifeelwinni.2112@gmail.com>
2022-09-11 21:00:13 +1000

    fix: after review 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,38 @@
+// open popup const
 const popup = document.querySelector('.popup');
-const editButton = document.querySelector('.profile__edit-button-img');
+const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__button-close');
 
-const togglePopup = function () {
-    popup.classList.toggle('popup_opened');
-}
-
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-
-
-let formElement = document.querySelector('.popup__content');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-about');
+// form submit let
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_name_text');
+let jobInput = document.querySelector('.popup__input_about_text');
 let nameProfile = document.querySelector('.profile__title');
 let jobProfile = document.querySelector('.profile__subtitle');
-let saveButton = document.querySelector('.popup__button-save');
 
+// open popup and popup value
+function openPopup () {
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
+    popup.classList.add('popup_opened');
+}
+
+// close popup
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
+
+// form submit
 function formSubmitHandler (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
+    closePopup();
 }
 
+//  listener open/close popup
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+
+// listener form
 formElement.addEventListener('submit', formSubmitHandler);
-saveButton.addEventListener('click', formSubmitHandler);
-saveButton.addEventListener('click', togglePopup);
